870cb875c86f0b17b7d4d01640613a82d81b8e36
RevealSquid <hubertle43100@gmail.com>
2021-08-13 17:47:49 -0700

    Initial commit

---
+++ SlearnUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlearnUI
+//
+//  Created by Hubert Le on 8/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
