0bbd39864eb06439c47f9d89b53d47099bf5422e
RevealSquid <hubertle43100@gmail.com>
2021-08-17 10:35:51 -0700

    make two devices and set up header

--- SlearnUI/ContentView.swift
+++ SlearnUI/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    //var pizza: Int
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //Text(pizza > 5 ? "Helloo Pizza!!!!" : "Hello World")
+        VStack {
+            Text("Huli Pizza Company")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Image("Surf Board")
+                .resizable()
+                .scaledToFit()
+            Text("Order Pizza")
+                .font(.largeTitle)
+            Spacer()
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .colorScheme(.dark)
+                .background(Color.black)
+                .previewDevice("iPad Pro (9.7-inch)")
+            //ContentView(pizza: 5)
+        }
     }
 }
