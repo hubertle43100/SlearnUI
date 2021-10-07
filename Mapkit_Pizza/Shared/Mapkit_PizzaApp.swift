291e3ab061f919a73c46a1ef5d3e2075f8c68bd1
RevealSquid <hubertle43100@gmail.com>
2021-10-06 21:03:29 -0700

    organize folder and started Mapkit course

---
+++ Mapkit_Pizza/Shared/Mapkit_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mapkit_PizzaApp.swift
+//  Shared
+//
+//  Created by Hubert Le on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Mapkit_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
