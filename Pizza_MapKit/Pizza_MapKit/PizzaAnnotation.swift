7e6997a77021e7e56026ae2cd23902f7c7894e2e
RevealSquid <hubertle43100@gmail.com>
2021-10-20 19:04:32 -0700

    Learned about making annotation pins for map

--- Pizza_MapKit/Pizza_MapKit/PizzaAnnotation.swift
+++ Pizza_MapKit/Pizza_MapKit/PizzaAnnotation.swift
@@ -9,6 +9,16 @@
 import UIKit
 import MapKit
 
-class PizzaAnnotation: NSObject {
-
+class PizzaAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    var identifier = "Pin"
+    var historyText = ""
+    var pizzzaPhoto: UIImage! = nil
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?){
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
 }
