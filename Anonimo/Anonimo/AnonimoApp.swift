b4e2b1440a8c18d257ba5b3e5364f409859ad8aa
RevealSquid <hubertle43100@gmail.com>
2021-12-03 23:35:48 -0800

    Starting new project

---
+++ Anonimo/Anonimo/AnonimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnonimoApp.swift
+//  Anonimo
+//
+//  Created by Hubert Le on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AnonimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
